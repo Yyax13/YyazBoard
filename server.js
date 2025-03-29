d2c86b82d3bf069b1c88ce8a1a87504882eea54a
Yyax13 <Yyax13@proton.me>
2025-03-29 21:23:26 +0000

    Versão ALPHA 0.0.1

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config(); //puxa dados do arquivo .env
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 3001;
+
+function clearHTML(BadString) {
+    return sanitizeHtml(BadString, {
+        allowedTags: [],
+        allowedAttributes: {}
+    });
+}
+
+/*
+
+
+Páginas estaticas
+
+
+*/
+
+app.get('/', (req, res) => {
+    res.send("Página em desenvolvimento, se preferir você pode enviar uma request GET em /api/consulta com os parametros query username e userid")
+});
+
+/*
+
+
+API
+
+
+*/
+
+app.get('/api/consulta', (req, res) => {
+    const { username, userid } = req.query;
+    res.send(`Olá ${clearHTML(username)}! O seu id é: ${clearHTML(userid)}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}!`);
+});
